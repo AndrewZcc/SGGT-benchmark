c84457e4d1ab849c9945b1fbb1510d06c922da6f
zhchuch_m <zcckernal@gmail.com>
2017-01-07 11:10:03 +0800

    append related files

---
+++ anymemo-stable-8.3/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/vagrant/subjects/tool_subjects/swifthand/anymemo-stable-8.3/bin/AndroidManifest.xml : \
+/vagrant/subjects/tool_subjects/swifthand/anymemo-stable-8.3/AndroidManifest.xml \
---
+++ anymemo-stable-8.3/bin/MainTabs-debug-unaligned.apk.d
@@ -0,0 +1,9 @@
+/vagrant/subjects/tool_subjects/swifthand/anymemo-stable-8.3/bin/MainTabs-debug-unaligned.apk : \
+/vagrant/subjects/tool_subjects/swifthand/anymemo-stable-8.3/bin/MainTabs.ap_ \
+/vagrant/subjects/tool_subjects/swifthand/anymemo-stable-8.3/bin/classes.dex \
+/home/vagrant/android-sdk-linux/tools/support/annotations.jar \
+/vagrant/subjects/tool_subjects/swifthand/anymemo-stable-8.3/libs/httpmime-4.1.2.jar \
+/vagrant/subjects/tool_subjects/swifthand/anymemo-stable-8.3/libs/httpcore-4.1.2.jar \
+/vagrant/subjects/tool_subjects/swifthand/anymemo-stable-8.3/libs/apache-mime4j-core-0.7.jar \
+/vagrant/subjects/tool_subjects/swifthand/anymemo-stable-8.3/libs/httpclient-4.1.2.jar \
+/home/vagrant/android-sdk-linux/tools/lib/emma_device.jar \
